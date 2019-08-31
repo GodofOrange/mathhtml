567e8fa462dccff27a8483450bb638e517adaacd
GodofOrange <wochidoubao123@163.com>
2019-09-01 00:29:09 +0800

    哈哈哈哈

--- src/components/together/myBigClass/ChatRoom.vue
+++ src/components/together/myBigClass/ChatRoom.vue
@@ -1,10 +1,51 @@
 <template>
-    <div>聊天室</div>
+  <div>
+    <input type="text" v-model="text">
+    <button @click="sendMessage">发送消息</button>
+    <div>
+      <div v-for="(data,key) in datas" :key="key">
+        {{data.content}}
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
+import SockJS from 'sockjs-client'
+import Stomp from 'webstomp-client'
 export default {
-  name: 'ChatRoom'
+  name: 'ChatRoom',
+  data () {
+    return {
+      text: '',
+      datas: [],
+      stompClient: null
+    }
+  },
+  mounted () {
+    if ('WebSocket' in window) {
+      this.initWebSocket()
+    } else {
+      alert('当前浏览器 Not support websocket')
+    }
+  },
+  methods: {
+    sendMessage () {
+      this.stompClient.send('/app/hello', JSON.stringify(this.text), {})
+    },
+    initWebSocket () {
+      this.connection()
+    },
+    connection () {
+      const socket = new SockJS(this.$baseUrl + '/chat')
+      this.stompClient = Stomp.over(socket)
+      this.stompClient.connect({}, (frame) => {
+        this.stompClient.subscribe('/topic/greetings', (greeting) => {
+          console.log(JSON.parse(greeting.body))
+          this.datas.push(JSON.parse(greeting.body))
+        })
+      })
+    }
+  }
 }
 </script>
 
